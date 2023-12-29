f95b4c09b07b1c164aabf64eb964890afb0ee696
amc19jhb <amal.chaouite.jhb@fh-zwickau.de>
2023-12-29 02:33:34 +0100

    added delete option

--- todoList-app-frontend/src/components/Task.jsx
+++ todoList-app-frontend/src/components/Task.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import classes from './Task.module.css'
 import { SlCheck } from "react-icons/sl";
+import { VscTrash } from "react-icons/vsc";
 function Task (props){
     const [isCompleted,setIsCompleted]= useState(false);
     function onComplete(){
         setIsCompleted(!isCompleted);
+    }
+
+    function onDelete(){
+        //TODO send a delete
     }
     return (
         <div className={classes.task}>
@@ -18,8 +23,11 @@ function Task (props){
                     <button type='button' className={classes.updateButton} onClick={props.onOpen}>update</button>
                 </div>
                 <div>
-                {isCompleted && <button className={classes.iconButton}  onClick={onComplete}><SlCheck style={{'color': 'green' }} size={24} /></button>}
-                {!isCompleted && <button className={classes.iconButton}  onClick={onComplete}><SlCheck style={{'color': 'grey' }} size={24} /></button>}
+                    {isCompleted && <button className={classes.iconButton}  onClick={onComplete}><SlCheck style={{'color': 'green' }} size={24} /></button>}
+                    {!isCompleted && <button className={classes.iconButton}  onClick={onComplete}><SlCheck style={{'color': 'grey' }} size={24} /></button>}
+                </div>
+                <div>
+                    <VscTrash className={classes.deleteIcon} size={24} onClick={onDelete} />
                 </div>
             </div>
             
